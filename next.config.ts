e142d23f8dc1a8d19a3876815b9b05ba6c8e7378
imafnan <afnanalamanan99@gmail.com>
2025-08-24 22:19:46 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -9,28 +9,33 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'placehold.co',
-      port: '',
-      pathname: '/**',
-    },
-    {
-      protocol: 'https',
-      hostname: 'www.wypazanotim.tv',
-      port: '',
-      pathname: '/**',
-    },
-    {
-      protocol: 'https',
-      hostname: 'www.feb.cm',
-      port: '',
-      pathname: '/**',
-    },
-  ],
-},
-
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.wypazanotim.tv',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.feb.cm',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloudcoffee.com.bd',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
